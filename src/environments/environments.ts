f173a48e49204cc0ff40a42c9d1c448bd99c4596
Mariel Torres <marieltorres97@gmail.com>
2023-01-14 21:17:14 -0300

    medias

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environments = {
     production: false,
-     /* apiURL: 'http://localhost:8080/'  */
+     /* apiURL: 'http://localhost:8080/' */ 
      apiURL: 'https://acute-greer-mitdesarrollo.koyeb.app/' 
 }
